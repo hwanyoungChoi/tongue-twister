b1b406f6496a44d47cb92e774bfe7098698a82d7
hwanyoungChoi <welcomechoi@kakao.com>
2026-02-16 22:50:02 +0900

    feat: build date 표시

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import AppLayout from "./containers/AppLayout";
 import Game from "./containers/game";
 
 function App() {
+  const buildDate = new Date(__APP_BUILD_DATE__).toLocaleString();
+
   return (
-    <AppLayout>
-      <AppEntry>
-        <Game />
-      </AppEntry>
-    </AppLayout>
+    <div className="relative">
+      <div className="absolute bottom-2 left-2 text-xs text-gray-500">
+        Build Date👅: {buildDate}
+      </div>
+      <AppLayout>
+        <AppEntry>
+          <Game />
+        </AppEntry>
+      </AppLayout>
+    </div>
   );
 }
 
