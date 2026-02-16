bf27becedb2c9db65153aa8e52b0db8fdeb74ca0
hwanyoungChoi <welcomechoi@kakao.com>
2026-02-16 22:49:32 +0900

    feat: vite-plugin-svgr

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+declare module "*.svg?react" {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
