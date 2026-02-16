ea01da1c3051aae19d63efb3250c4bd9e892920d
hwanyoungChoi <welcomechoi@kakao.com>
2026-02-16 17:09:22 +0900

    feat: splash 이미지 추가 및 앱 최소너비 설정

--- src/containers/AppEntry.tsx
+++ src/containers/AppEntry.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import ImageLogo from "../assets/images/logo.png";
+import ImageSplash2 from "../assets/images/splash-2.png";
 
 export default function AppEntry({ children }: { children: React.ReactNode }) {
   const [splashStep, setSplashStep] = useState(0); // 0: first splash, 1: seconds splash, 2: done
@@ -22,21 +23,25 @@ export default function AppEntry({ children }: { children: React.ReactNode }) {
 
   if (splashStep === 0) {
     return (
-      <div className="min-h-dvh flex flex-col items-center justify-center font-one-pop">
-        <img src={ImageLogo} alt="팅틀러 로고" className="w-[163px] h-auto" />
+      <div className="min-h-dvh flex flex-col items-center justify-center font-one-pop bg-white">
+        <img src={ImageLogo} alt="텅틀려 로고" className="w-[163px] h-auto" />
       </div>
     );
   }
 
   if (splashStep === 1) {
     return (
-      <div className="min-h-dvh flex flex-col items-center justify-center font-one-pop">
-        <h1>
+      <div className="min-h-dvh flex flex-col items-center justify-center font-one-pop bg-white">
+        <h1 className="text-center text-[28px] text-[#1F1F1F]">
           실수 없이
           <br />
           말할 수 있겠어?
         </h1>
-        <div className="w-[186px] h-[252px] mt-[15px] bg-pink-100" />
+        <img
+          src={ImageSplash2}
+          alt="텅틀려 스플래시 캐릭터"
+          className="w-[186px] h-auto mt-[15px]"
+        />
       </div>
     );
   }
--- src/containers/AppLayout.tsx
+++ src/containers/AppLayout.tsx
@@ -1,6 +1,6 @@
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-dvh max-w-[480px] mx-auto flex flex-col">
+    <div className="min-h-dvh max-w-[480px] min-w-[375px] mx-auto flex flex-col">
       {children}
     </div>
   );
