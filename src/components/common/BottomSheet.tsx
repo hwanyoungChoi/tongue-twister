db48dff3a87ce012323aefa586999a0e9e3073ea
hwanyoungChoi <welcomechoi@kakao.com>
2026-02-18 14:09:33 +0900

    feat: drawer, bottom sheet

---
+++ src/components/common/BottomSheet.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import IconX from "@/assets/icons/x.svg?react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerClose,
+  DrawerFooter,
+} from "@/components/ui/drawer";
+
+interface BottomSheetProps {
+  open: boolean;
+  close: () => void;
+  dismissible?: boolean;
+  title?: string;
+  content?: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+export function BottomSheet({
+  open,
+  close,
+  dismissible = true,
+  title,
+  content,
+  footer,
+}: BottomSheetProps) {
+  return (
+    <Drawer open={open} dismissible={dismissible}>
+      <DrawerContent className="rounded-t-[20px] max-w-[480px] min-w-[375px] mx-auto">
+        {/* 헤더: 타이틀 & 닫기 버튼 */}
+        <DrawerHeader className="relative flex flex-row items-center justify-between px-[24px] h-[74px]">
+          <DrawerTitle className="text-[21px] font-bold text-[#1F1F1F] text-left">
+            {title}
+          </DrawerTitle>
+          <DrawerClose asChild>
+            <button
+              className="text-gray-400 hover:text-gray-600"
+              onClick={close}
+            >
+              <IconX className="w-6 h-6" />
+            </button>
+          </DrawerClose>
+        </DrawerHeader>
+
+        {/* 본문: 설명 & 입력창 */}
+        {content}
+
+        {/* 푸터: 적용하기 버튼 */}
+        <DrawerFooter className="px-5 pt-4">{footer}</DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
