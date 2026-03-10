9866d3f0f8739076ea29c6d30a6f46e0e62562d1
hwanyoungChoi <welcomechoi@kakao.com>
2026-03-10 11:00:27 +0900

    feat: 게임 방법 팝업 컨텐츠 추가

--- src/components/common/RulePopup.tsx
+++ src/components/common/RulePopup.tsx
@@ -1,16 +1,46 @@
 import { Popup, type PopupProps } from "./Popup";
 
+import ImageBasicRules from "@/assets/images/rule/basic_rules.svg?react";
+import ImageLong from "@/assets/images/rule/long.svg?react";
+import ImageShort from "@/assets/images/rule/short.svg?react";
+import ImageTimer from "@/assets/images/rule/timer.svg?react";
+import ImageConscience from "@/assets/images/rule/conscience.svg?react";
+
 export default function RulePopup({
   open,
   onOpenChange,
 }: Pick<PopupProps, "open" | "onOpenChange">) {
   return (
-    <Popup open={open} onOpenChange={onOpenChange} title="게임 방법">
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="font-np text-[40px] leading-[1.4] text-[#1F1F1F]">
-          게임 방법
-        </h2>
-        <span className="text-[32px]">🚧</span>
+    <Popup
+      open={open}
+      onOpenChange={onOpenChange}
+      title="게임 방법"
+      width="335px"
+      height="100%"
+    >
+      <div className="px-[16px] pb-[48px] space-y-[32px] overflow-y-auto h-full">
+        <div>
+          <h3 className="font-[800] text-[16px] text-[#1F1F1F] leading-[1.6] mb-[8px]">
+            기본 규칙
+          </h3>
+          <ImageBasicRules />
+        </div>
+        <div>
+          <h3 className="font-[800] text-[16px] text-[#1F1F1F] leading-[1.6] mb-[8px]">
+            문구 길이
+          </h3>
+          <div className="flex justify-center gap-[8px]">
+            <ImageLong />
+            <ImageShort />
+          </div>
+        </div>
+        <div>
+          <h3 className="font-[800] text-[16px] text-[#1F1F1F] leading-[1.6] mb-[8px]">
+            진행 방식
+          </h3>
+          <ImageTimer />
+          <ImageConscience />
+        </div>
       </div>
     </Popup>
   );
