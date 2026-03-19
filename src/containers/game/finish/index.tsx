d81fadc3a61d4d018c9c151fcc2e4ceacbc65995
hwanyoungChoi <welcomechoi@kakao.com>
2026-03-19 23:52:51 +0900

    ddd

--- src/containers/game/finish/index.tsx
+++ src/containers/game/finish/index.tsx
@@ -2,17 +2,25 @@ import FixedBottom from "@/components/common/FixedBottom";
 import Header from "@/components/common/Header";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/lib/routes";
+import useGameStore from "@/stores/useGameStore";
 import { useNavigate } from "react-router-dom";
 
 export default function GameFinish() {
   const navigate = useNavigate();
 
+  const penalty = useGameStore((state) => state.penalty);
+  const players = useGameStore((state) => state.players);
+
+  console.log(players);
+
   return (
     <div className="min-h-dvh bg-[#F8FAFA]">
       <Header type="finish" />
 
-      <main>
-        <p>탈락자 누구누구</p>
+      <main className="min-h-dvh bg-white flex">
+        <div className="flex flex-col flex-1 items-center justify-center -mt-[48px] bg-[red]">
+          {penalty ? <FinishPenalty penalty={penalty} /> : <FinishRanking />}
+        </div>
       </main>
 
       <FixedBottom>
@@ -37,4 +45,12 @@ export default function GameFinish() {
       </FixedBottom>
     </div>
   );
+}
+
+function FinishPenalty({ penalty }: { penalty: string }) {
+  return <div>{penalty}</div>;
+}
+
+function FinishRanking() {
+  return <div>랭킹</div>;
 }
